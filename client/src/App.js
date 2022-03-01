09d09702d454c5c8bd222449c864141926c708e6
Konner Hartman <konner.hartman@yahoo.com>
2022-03-01 12:53:30 -0600

    added starter code. Lots of bugs still.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
-  ApolloProvider,
-} from '@apollo/client';
 
 import Home from './pages/Home';
-import Gallery from '.pages/Gallery';
+import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
@@ -14,22 +11,20 @@ import { StoreProvider } from './utils/GlobalState';
 
 function App() {
   return (
-    <ApolloProvider>
-      <Router>
-        <div>
-          <StoreProvider>
-            <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/gallery" component={Gallery} />
-              <Route exact path="/contact" component={Contact} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
-            </Switch>
-          </StoreProvider>
-        </div>
-      </Router>
-    </ApolloProvider>
+    <Router>
+      <div>
+        <StoreProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/gallery" component={Gallery} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/products/:id" component={Detail} />
+            <Route component={NoMatch} />
+          </Switch>
+        </StoreProvider>
+      </div>
+    </Router>
   );
 }
 
