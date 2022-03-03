9621f29b6bc54e49d42788616be562ee8b180a07
Konner Hartman <konner.hartman@yahoo.com>
2022-03-03 12:57:03 -0600

    fixed nav links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,54 +7,20 @@ function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/about">
-              About
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/gallery">
-              Gallery
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/contact">
-              Contact
-            </Link>
-          </li>
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
-        </ul>
+        <li className="mx-1">
+          {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+          <a href="/" onClick={() => Auth.logout()}>
+            Logout
+          </a>
+        </li>
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/about">
-              About
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/gallery">
-              Gallery
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/contact">
-              Contact
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
+        <li className="mx-1">
+          <Link to="/login">
+            Login
+          </Link>
+        </li>
       );
     }
   }
@@ -66,9 +32,27 @@ function Nav() {
         Kota Bear Woodworks
         </Link>
       </h1>
-
+      
       <nav>
-        {showNavigation()}
+        <ul className="flex-row">
+          <li className="mx-1">
+              <Link to="/about">
+                About
+              </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/gallery">
+              Gallery
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/contact">
+              Contact
+            </Link>
+          </li>
+            {showNavigation()}
+        </ul>
+        
       </nav>
     </header>
   );
