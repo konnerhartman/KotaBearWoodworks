756c6ceea218fddb297efdf9748defbc7fe76bd9
Konner Hartman <konner.hartman@yahoo.com>
2022-03-03 18:03:03 -0600

    styled homepage with bootstrap

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Jumbotron from "../components/Jumbotron";
+// import Jumbotron from "../components/Jumbotron";
 
 
 const Home = () => {
   return (
-    <div className="container jumbo">
-      <Jumbotron>
-        <button>
-          <Link to="/gallery">
-            Gallery
-          </Link>
-        </button>
-      </Jumbotron>
+    <div className="jumbotron text-center p-5">
+      <h1 className="display-4">Hello!</h1>
+      <p className="lead">Welcome to Kota Bear's workshop.</p>
+      <p>Take a look at my gallery to see a few items I have built.</p>
+      <Link className="btn btn-primary btn-lg" to="/gallery">
+        Gallery
+      </Link>
     </div>
   );
 };
