c8691507360f4058b0c8ae67b6705d1edcb122b7
Konner Hartman <konner.hartman@yahoo.com>
2022-03-03 18:21:32 -0600

    styled footer with bootstrap

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 function Footer() {
 return (
   <div>
-    <footer className="text-center text-white fixed-bottom bg-dark">
-      <div className="container p-2"></div>
-
-      <div className="text-center p-3">
-        © 2022 Copyright
+    <footer className="text-white sticky-bottom bg-dark">
+      <div className="row px-2 pt-3">
+        <p className="col-6 text-start p-2">© 2022 Copyright</p>
+        <p className="col-6 text-end p-2">Website built by:
+          <a className="text-decoration-none" href="https://github.com/konnerhartman">Konner Hartman</a>
+        </p>
+      </div>
+      <div className="text-end">
+        
       </div>
     </footer>
   </div>
