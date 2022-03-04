a4f43e319e62421c22263e35cf78ccfc1fa32fe4
Konner Hartman <konner.hartman@yahoo.com>
2022-03-03 19:46:25 -0600

    started styling about page with bootstrap

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="container">
-      <h1>About page</h1>
+    <div className="about row">
+      <div className="col-6">
+
+      </div>
+      <aside className="p-3 col-6 text-center text-white">
+        <h2 className="p-2">About Kota Bear Woodworks</h2>
+        <p>My name is Kevin. I have been building with wood since I was a young child. My first jobs were building houses. I've always been a DIYer. As I aged and moved out of the construction business, I always kept at it by doing side projects for friends and family.</p>
+        <p>If you're wondering where the name "Kota Bear" comes from...</p>
+      </aside>
     </div>
   );
 };
