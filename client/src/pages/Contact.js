6d4a7d81be71229876a7594de568f72d9daef477
Konner Hartman <konner.hartman@yahoo.com>
2022-03-11 15:59:29 -0600

    contact form works

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,42 +1,60 @@
-import React from "react";
+import React from 'react';
+import emailjs from '@emailjs/browser';
 
-const About = () => {
+const Contact = () => {
+
+  function sendEmail(e) {
+
+    e.preventDefault();
+
+    emailjs.sendForm('service_cu1kehw', 'template_3p56679', e.target, 'sGi0Fy7UX2yCkogHD')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
+
   return (
     <div className="contact row">
-      <section class="mb-4 text-white">
+      <section className="mb-4 text-white">
         <div>
-          <div class="col-md-6 p-3">
-            <h2 class="h1-responsive font-weight-bold text-center my-4">Reach Out</h2>
-            <p class="text-center w-responsive mx-auto">Do you have any questions about pricing? Want to discuss custom pieces? Please do not hesitate to contact me directly.</p>
-            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-              <div class="row mb-3">
-                <div class="col-md-6">
-                  <div class="md-form mb-0">
-                    <input type="text" id="name" name="name" class="form-control" />
-                    <label for="name" class="">Your name</label>
+          <div className="col-md-6 p-3">
+            <h2 className="h1-responsive font-weight-bold text-center my-4">Reach Out</h2>
+            <p className="text-center w-responsive mx-auto">Do you have any questions about pricing? Want to discuss custom pieces? Please do not hesitate to contact me directly.</p>
+            <form onSubmit={sendEmail} id="contact-form" name="contact-form" action="mail.php" method="POST">
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <div className="md-form mb-0">
+                    <input type="text" id="name" name="name" className="form-control" />
+                    <label className="">Your name</label>
                   </div>
                 </div>
-                <div class="col-md-6">
-                  <div class="md-form mb-0">
-                    <input type="text" id="email" name="email" class="form-control" />
-                    <label for="email" class="">Your email</label>
-                  </div>
-                </div>
-              </div>
-              <div class="row  mb-3">
-                <div class="col-md-12">
-                  <div class="md-form mb-0">
-                    <input type="text" id="subject" name="subject" class="form-control" />
-                    <label for="subject" class="">Subject</label>
+                <div className="col-md-6">
+                  <div className="md-form mb-0">
+                    <input type="text" id="email" name="email" className="form-control" />
+                    <label className="">Your email</label>
                   </div>
                 </div>
               </div>
-              <div class="row">
-                <div class="col-md-12">
-                  <div class="md-form">
-                    <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                    <label for="message">Your message</label>
+              <div className="row  mb-3">
+                <div className="col-md-12">
+                  <div className="md-form mb-0">
+                    <input type="text" id="subject" name="subject" className="form-control" />
+                    <label className="">Subject</label>
                   </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="md-form">
+                    <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea"></textarea>
+                    <label>Your message</label>
+                  </div>
+                </div>
+                <div>
+                  <input type="submit" value="send" />
                 </div>
               </div>
             </form>
@@ -49,4 +67,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Contact;
