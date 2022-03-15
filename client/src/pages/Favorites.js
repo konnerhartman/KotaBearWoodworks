ecf1e171798c7ff4a2637553ad36e2dd8b80fd07
Konner Hartman <konner.hartman@yahoo.com>
2022-03-15 11:04:39 -0500

    favorites page now renders liked items

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { idbPromise } from '../utils/helpers';
+import CartItem from '../components/CartItem';
+import { useStoreContext } from '../utils/GlobalState';
+import { ADD_MULTIPLE_TO_CART } from '../utils/actions';
 
+const Favorites = () => {
+  const [state, dispatch] = useStoreContext();
 
-function Favorites() {
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+    }
 
-}
+    if (!state.cart.length) {
+      getCart();
+    }
+  }, [state.cart.length, dispatch]);
+
+  return (
+    <div className="bg-light">
+      <h2 className='px-2 text-center pb-3'>Liked items</h2>
+      {state.cart.length ? (
+        <div>
+          {state.cart.map((item) => (
+            <CartItem key={item._id} item={item} />
+          ))}
+        </div>
+      ) : (
+        <h3>
+          You haven't liked anything yet!
+        </h3>
+      )}
+    </div>
+  );
+};
 
 export default Favorites;
