38d020ffe69d751067a8e33051dd6149f3d6e061
Konner Hartman <konner.hartman@yahoo.com>
2022-03-16 14:00:28 -0500

    added availability to seed data

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -21,6 +21,7 @@ db.once('open', async () => {
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'cookie-tin.jpg',
+      availabity: 'Yes',
       category: categories[0]._id,
     },
     {
@@ -28,6 +29,7 @@ db.once('open', async () => {
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
       image: 'canned-coffee.jpg',
+      availabity: 'Yes',
       category: categories[0]._id,
     },
     {
@@ -36,6 +38,7 @@ db.once('open', async () => {
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
       image: 'toilet-paper.jpg',
+      availabity: 'Yes',
     },
     {
       name: 'Handmade Soap',
@@ -43,6 +46,7 @@ db.once('open', async () => {
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
       image: 'soap.jpg',
+      availabity: 'Yes',
     },
     {
       name: 'Set of Wooden Spoons',
@@ -50,6 +54,7 @@ db.once('open', async () => {
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
       image: 'wooden-spoons.jpg',
+      availabity: 'No',
     },
     {
       name: 'Camera',
@@ -57,6 +62,7 @@ db.once('open', async () => {
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'camera.jpg',
+      availabity: 'Yes',
     },
     {
       name: 'Tablet',
@@ -64,6 +70,7 @@ db.once('open', async () => {
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
       image: 'tablet.jpg',
+      availabity: 'Yes',
     },
     {
       name: 'Tales at Bedtime',
@@ -71,6 +78,7 @@ db.once('open', async () => {
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
       image: 'bedtime-book.jpg',
+      availabity: 'No',
     },
   ]);
 
