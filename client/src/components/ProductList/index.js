820edbc7054cacc8ad14b705b863f400c781251c
Konner Hartman <konner.hartman@yahoo.com>
2022-03-17 15:29:02 -0500

    adjusted mobile liked items button

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -61,7 +61,7 @@ function ProductList() {
           ))}
         </div>
       ) : (
-        <h3>You haven't added any products yet!</h3>
+        <h3>I haven't added any products yet!</h3>
       )}
       {loading ? <FontAwesomeIcon className='spinner' icon={faScrewdriverWrench} /> : null}
     </div>
