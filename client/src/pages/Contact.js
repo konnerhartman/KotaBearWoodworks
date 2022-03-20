9d9ecaf7b01fc395acd02be34552a93a4c30b082
Konner Hartman <konner.hartman@yahoo.com>
2022-03-20 14:07:53 -0500

    fixed spacing on contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -19,54 +19,54 @@ const Contact = () => {
   };
 
   return (
-    <div className="contact row">
+    <div className="contact row text-cetner">
       <section className="mb-4 text-white">
-        <div>
-          <div className="col-md-6 p-3">
-            <h2 className="h1-responsive font-weight-bold text-center my-4"><span>Reach Out</span></h2>
-            <p className="text-center w-responsive mx-auto">Do you have any questions about pricing? Want to discuss custom pieces? Please do not hesitate to contact me directly.</p>
-            <form onSubmit={sendEmail} id="contact-form" name="contact-form" action="mail.php" method="POST">
-              <div className="row mb-3">
-                <div className="col-md-6">
-                  <div className="md-form mb-0">
-                    <input type="text" id="name" name="name" className="form-control" required />
-                    <label className="">Your name</label>
+        <div className='container'>
+          <div className='row'>
+            <div className="col-md-6 p-3">
+              <h2 className="h1-responsive font-weight-bold text-center my-4"><span>Reach Out</span></h2>
+              <p className="text-center w-responsive mx-auto">Do you have any questions about pricing? Want to discuss custom pieces? Please do not hesitate to contact me directly.</p>
+              <form onSubmit={sendEmail} id="contact-form" name="contact-form" action="mail.php" method="POST">
+                <div className="row mb-3">
+                  <div className="col-md-6">
+                    <div className="md-form mb-0">
+                      <input type="text" id="name" name="name" className="form-control" required />
+                      <label className="">Your name</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="md-form mb-0">
+                      <input type="text" id="email" name="email" className="form-control" required />
+                      <label className="">Your email</label>
+                    </div>
                   </div>
                 </div>
-                <div className="col-md-6">
-                  <div className="md-form mb-0">
-                    <input type="text" id="email" name="email" className="form-control" required />
-                    <label className="">Your email</label>
+                <div className="row  mb-3">
+                  <div className="col-md-12">
+                    <div className="md-form mb-0">
+                      <input type="text" id="subject" name="subject" className="form-control" required />
+                      <label className="">Subject</label>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="row  mb-3">
-                <div className="col-md-12">
-                  <div className="md-form mb-0">
-                    <input type="text" id="subject" name="subject" className="form-control" required />
-                    <label className="">Subject</label>
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="md-form">
+                      <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea" required></textarea>
+                      <label>Your message</label>
+                    </div>
+                  </div>
+                  <div className='text-end'>
+                    <button className='btn btn-secondary px-4' type="submit">
+                      Send <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
                   </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="md-form">
-                    <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea" required></textarea>
-                    <label>Your message</label>
-                  </div>
-                </div>
-                <div className='text-end'>
-                  <button className='btn btn-secondary px-4' type="submit">
-                    Send <FontAwesomeIcon icon={faPaperPlane} />
-                  </button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </section>
-      <div className="col-6">
-      </div>
     </div>
   );
 };
