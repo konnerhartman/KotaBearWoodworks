0653ad29a20d5319c395d09089001c6950659c00
Konner Hartman <konner.hartman@yahoo.com>
2022-03-20 13:45:30 -0500

    fixed spacing on about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,14 +3,12 @@ import React from "react";
 const About = () => {
   return (
     <div className="about row">
-      <div className="col-6">
-
-      </div>
+      <div className="col-5"></div>
       <aside className="p-3 col-6 text-center text-white">
         <h2 className="p-2"><span>About Kota Bear Woodworks</span></h2>
-        <p>My name is Kevin. I have been building with wood since I was a young child. My first jobs were building houses. I've always been a DIYer. As I aged and moved out of the construction business, I always kept at it by doing side projects for friends and family.</p>
-        <p>If you're wondering where the name "Kota Bear" comes from...</p>
+        <p>My name is Kevin. I have been building with wood since I was a young man. My first jobs were building houses. I've always been a DIYer. As I aged and moved out of the construction business, I always kept at it by doing side projects for friends and family. Woodworking is my true passion and I enjoy being able to bring others ideas to life.</p>
       </aside>
+      <div className="col-1"></div>
     </div>
   );
 };
